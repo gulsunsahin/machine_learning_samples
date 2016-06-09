2868bd5a361eedf011eaa7914458fd4f6be9d122
unknown <gulsun.sahin@GULSUNSAHIN.etiyacorp.local>
2016-06-09 11:02:42 +0300

    cross validation

--- multiple_text_feature/main_cross_validation.py
+++ multiple_text_feature/main_cross_validation.py
@@ -40,78 +40,26 @@ numeric_topics = [name.replace('Mortgage', '1').replace('Debt collection', '2').
 numeric_topics = [float(i) for i in numeric_topics]
 
 Y = np.array(numeric_topics)
-X = zip(*[feature, question, feature_1])
+X = list(zip(*[feature, question, feature_1]))
 
-#from sklearn.feature_extraction.text import TfidfVectorizer
-#vectorizer = TfidfVectorizer()
-#vectors = vectorizer.fit_transform(question)
+from sklearn.cross_validation import train_test_split
 
-#print(vectors)
+X_train, X_test, y_train, y_test = train_test_split(X, topic, test_size=0.10, random_state=30)
 
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer =  CountVectorizer(tokenizer=lambda doc: doc, lowercase=False)
-bag_of_words =vectorizer.fit_transform(X)
+bag_of_words =vectorizer.fit_transform(X_train)
 
 clf = MultinomialNB(alpha=.01) 
-clf.fit(bag_of_words, Y, sample_weight=10)
+clf.fit(bag_of_words, y_train, sample_weight=10)
 
-clf_preds = clf.predict_proba(bag_of_words)
-
-print(clf_preds)
-
-vectors_test_data=[]
-vectors_test_expected_result=[]
-feature_test = []
-feature_1_test = []
-
-with open('test_data.txt') as f:
-	for line in f:
-		data = line.split(';')
-		vectors_test_expected_result.append(data[0])
-		vectors_test_data.append(data[1])
-		if "mortgage" in data[1]:
-			feature_test.append("101")
-		elif ("dept" in data[1]) and ("collection" in data[1]):
-			feature_test.append("102")
-		elif ("credit" in data[1]) and ("reporting" in data[1]):
-			feature_test.append("103")
-		elif ("student" in data[1]) and ("loan" in data[1]):
-			feature_test.append("104")
-		elif ("bank" in data[1]) and ("account" in data[1]):
-			feature_test.append("105")
-		elif ("consumer" in data[1]) and ("loan" in data[1]):
-			feature_test.append("106")
-		else :
-			feature_test.append("107")
-
-		if ("Bank of America" in data[1]):
-			feature_1_test.append("creditreporting")
-		else:
-			feature_1_test.append("ZZZZ")
-
-numeric_result_topics = [name.replace('Mortgage', '1').replace('Debt collection', '2').replace('Credit reporting', '3').replace('Consumer Loan', '4').replace('Bank account or service', '5').replace('Money transfers', '6').replace('Credit card', '7').replace('Student loan', '8').replace('Payday loan', '9').replace('Prepaid card', '10').replace('Other financial service', '11') for name in vectors_test_expected_result]
-numeric_result_topics = [float(i) for i in numeric_result_topics]
-
-X = zip(*[feature_test, vectors_test_data, feature_1_test])
-
-vectors_test = vectorizer.transform(X)
+vectors_test = vectorizer.transform(X_test)
 pred = clf.predict(vectors_test)
 
 numberFalse=0
 for index in range(len(pred)):
-	if (pred[index] != numeric_result_topics[index]):
+	if (pred[index] != y_test[index]):
 		numberFalse=numberFalse+1;
 	
-print ("200 veriden yanlis hesaplanan sayisi:")
-print(numberFalse)
-print("")
-print(pred)
-
-from sklearn.metrics import *
-res = confusion_matrix(numeric_result_topics, pred)
-print(res)
-
-from sklearn import metrics
-acc = metrics.accuracy_score(numeric_result_topics, pred)
-print(acc) # 0.793 = (184 + 54)/ (184 + 17 + 45 + 54)
-
+print("Yuzde degeri:" + str((numberFalse*100)/len(pred)))
+print (str(len(pred)) + " ==> veriden yanlis hesaplanan sayisi:" + str(numberFalse))
